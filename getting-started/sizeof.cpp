b7d4abaaf1207019c51c9dd94f46dcb40fd833c2
Chaitan Bandela <hello@chaitan.com>
2023-01-22 20:39:39 -0800

    sizeof pointers

--- getting-started/sizeof.cpp
+++ getting-started/sizeof.cpp
@@ -6,21 +6,25 @@ int main()
 {
     int num;
     int nums[50];
+    int *nump;
     float decimal;
     bool isTrue;
     unsigned int max;
     char letter;
+    char *letterp;
     double pi;
     short int number;
     char letters[26];
 
     cout << "sizeof int: " << sizeof num << endl;
     cout << "sizeof int[50] array: " << sizeof nums << endl;
+    cout << "size of int* pointer: " << sizeof nump << endl;
     cout << "sizeof short int: " << sizeof number << endl;
     cout << "sizeof unsigned int: " << sizeof max << endl;
     cout << "sizeof float: " << sizeof decimal << endl;
     cout << "sizeof double: " << sizeof pi << endl;
     cout << "sizeof char: " << sizeof letter << endl;
+    cout << "sizeof char*: " << sizeof letterp << endl;
     cout << "sizeof char[26] array: " << sizeof letters << endl;
     cout << "sizeof bool: " << sizeof isTrue << endl;
 
